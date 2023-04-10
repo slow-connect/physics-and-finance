9e1bb9164b1792ea404f7f6519beadad1fa64cd0
slow-connect <louisdeberlin+github@gmail.com>
2023-04-10 18:28:23 +0200

    portfolio_selection_completed

--- portfolio-selection.py
+++ portfolio-selection.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 from scipy.linalg import inv
 
 # to ignore warings when handling dataframes
-import warnings
-warnings.filterwarnings("ignore")
+# import warnings
+# warnings.filterwarnings("ignore")
 
+
+# debugging for better readability on the terminal
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 
 
@@ -17,25 +19,26 @@ trading_days = 252
 
 def plot(stock_data, name, stocjs):
 
-    plt.figure(1)
+    plt.figure()
     for stock in stocks:
 
         plt.plot(stock_data[stock + name], label=stock, linewidth=0.7)
     plt.legend()
 
     if name == '':
-        plt.figure(2)
-        plt.plot(stock_data['Apple'], label='Apple')
-        plt.plot(stock_data['Coke'], label='Coke')
+        plt.figure()
+        plt.plot(stock_data['Apple'], label='Apple', linewidth=0.7)
+        plt.plot(stock_data['Coke'], label='Coke', linewidth=0.7)
         plt.legend()
 
-        plt.figure(3)
-        plt.plot(stock_data['Coke'], label='Coke')
+        plt.figure()
+        plt.plot(stock_data['Coke'], label='Coke', linewidth=0.7)
         plt.legend()
-    plt.show()
+    # plt.show()
 
 ## part a)
 stocks = ['Apple', 'SP500', 'Coke']
+plot(stock_data, '', stocks)
 for stock in stocks:
     stock_data[stock + 'shifted'] = stock_data[stock].shift(-1)
 
@@ -45,20 +48,18 @@ stock_data.drop(stock_data.index[-1], inplace=True)
 for stock in stocks:
     stock_data[stock + 'Day-to-Day_return'] = stock_data.apply(lambda row: (row[stock + 'shifted'] - row[stock]) / row[stock], axis=1)
 
+
 # plot day to day return: uncomment following line
-# plot(stock_data, 'Day-to-Day_return', stocks)
+plot(stock_data, 'Day-to-Day_return', stocks)
 
 
 # caluclate average of day to day
 average_return = np.zeros(3)
 cnt=0
 for stock in stocks:
-    # prGreen(stock)
-    # print(stock_data[stock + 'Day-to-Day_return'].mean())
     locals()[stock+'average'] = stock_data[stock + 'Day-to-Day_return'].mean()
     average_return[cnt] = stock_data[stock + 'Day-to-Day_return'].mean()
     cnt += 1
-# print(average_return)
 
 # calculate the covariance matrix
 for stock in stocks:
@@ -78,14 +79,13 @@ def plt_volatility_return(covariant_matrix, average_return, stocks):
     plt.legend()
     plt.xlabel('volatility')
     plt.ylabel('return')
-    plt.show()
 
 # plot volatily of individual stocks: uncomment following line
-# plt_volatility_return(covariant_matrix, average_return, stocks)
+plt_volatility_return(covariant_matrix, average_return, stocks)
 
 
 # Apply Markowitz Theory without risk free assets.
-returns = [0.05, 0.1, 0.15]
+returns = np.array([0.05, 0.1, 0.15])/trading_days
 e = np.ones(3)
 e3 = np.zeros(3)
 e3[2] = 1
@@ -93,7 +93,6 @@ e3[2] = 1
 def portfolio(rtrn, inv_covariant_matrix, v, inv_A):
     w = np.array([rtrn, 1, 0.3])
     lambdas = dot(inv_A, w)
-    # print(lambdas)
     weights = lambdas[0]*np.matmul(inv_covariant_matrix, v[0]) + lambdas[1]*np.matmul(inv_covariant_matrix, v[1]) + lambdas[2]*np.matmul(inv_covariant_matrix, v[2])
     return weights
 
@@ -107,21 +106,16 @@ def volatility(returns, inv_covariant_matrix, v, covariant_matrix):
     inv_A = inv(A_ij)
     for k in range(len(returns)):
         weights = portfolio(returns[k], inv_covariant_matrix, v, inv_A)
-        # print(weights)
         volatilities[k] = np.sqrt(np.matmul(weights, np.matmul(covariant_matrix, weights)))
     return volatilities
 
 
 plt.figure()
-# returns = np.linspace(0.001, 0.2, 100)
+returns = np.linspace(0, 0.275, 101)/trading_days
 
 v = np.array([average_return, e, e3])
 volatilities = volatility(returns, inv_covariant_matrix, v, covariant_matrix)
 
-print(list(zip(returns,volatilities)))
-print(list(zip(stocks, np.diagonal(covariant_matrix))))
-# plt.scatter(volatilities, returns)
-# plt_volatility_return(covariant_matrix, average_return, stocks)
-# plt.xlabel('volatility')
-# plt.ylabel('return')
-# plt.show()
+plt.plot(volatilities, returns)
+plt_volatility_return(covariant_matrix, average_return, stocks)
+plt.show()
