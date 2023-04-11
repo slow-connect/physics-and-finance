8a0622221673efd84c8d474ac24d084128d2836b
slow-connect <louisdeberlin+github@gmail.com>
2023-04-11 10:05:42 +0200

    Update integration.py

--- integration.py
+++ integration.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def psi(s, si, rho, t):
     a = 1/sqrt(2*pi*si*si*t)
     b = (-1) * (log(s) - rho*t)*(log(s) - rho*t) / (2*si*si*t)
-    return a * exp(b)
+    return a * exp(b)/s
 
 si = 0.3
 rho = 0.1
@@ -16,7 +16,7 @@ t = 2
 int = quad(psi, 0.4, 0.6, args=(si, rho, t))
 print(int)
 
-x = linspace(0.001, 5, 1001)
+x = linspace(0.0001, 5, 1001)
 y = np.zeros(len(x))
 for k in range(len(x)):
     y[k] = psi(x[k], si, rho, t)
